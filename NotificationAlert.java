56ae1ff6e79b73884edf1e4961b62fddd7015f05
blake-metro <blake.jordan@my.metrostate.edu>
2024-03-07 11:07:55 -0600

    Alert Classes (Abstract)

---
+++ NotificationAlert.java
@@ -0,0 +1,17 @@
+package weatherapp;
+
+import java.util.Date;
+
+public class NotificationAlert extends Alert {
+
+	public NotificationAlert(String name, String description, Date time) {
+		super(name, description, time);
+	}
+
+	@Override
+	void displayAlert() {
+		// do something
+		
+	}
+
+}
