26c7dc86228233f68d4aac47eeb8229bb84bb64e
ssebesta1 <156980642+ssebesta1@users.noreply.github.com>
2024-03-20 01:50:16 -0500

    Add files via upload

---
+++ WeatherApp/src/main/java/weathergroup/weatherapp/Location.java
@@ -0,0 +1,31 @@
+
+public class Location
+{
+
+	private String city;
+	private String zipCode;
+	private WeatherData weatherData;
+	
+	public Location(String city, String zipCode, WeatherData weatherData)
+	{
+		this.city = city;
+		this.zipCode = zipCode;
+		this.weatherData = weatherData;
+	}
+	
+	public String getCity() 
+	{
+        return city;
+    }
+
+    public String getZipCode() 
+    {
+        return zipCode;
+    }
+
+    public WeatherData getWeatherData() 
+    {
+        return weatherData;
+    }
+
+}
